24b334b970264f98ee44da661cda6b71e50ca769
waldohuanqui <131603641+waldohuanqui@users.noreply.github.com>
2023-05-08 19:18:53 -0500

    Delete Tarea03 directory

--- Tarea03/ejercicio1/main.py
+++
@@ -1,8 +0,0 @@
-import problema1
-import problema2
-import problema3
-
-if __name__ == '__main__':
-    problema1.resolver()
-    problema2.resolver()
-    problema3.resolver()
--- Tarea03/ejercicio7/ejercicio7.py
+++
@@ -1,9 +0,0 @@
-class Persona:
-    def __init__(self):
-        self.nombre = input("Ingresa tu nombre: ")
-        self.apellido = input("Ingresa tu appellido: ")
-        self.edad = int(input("Ingresa edad: "))
-
-
-persona1 = Persona()
-
